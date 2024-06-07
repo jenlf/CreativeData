ef7d22429ef4be1dbffcc6b9d2064484be6581f2
jenlf <fritz.jenniferlynn@gmail.com>
2024-06-07 15:46:48 -0400

    first commit on new project

---
+++ CreativeData/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CreativeData.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
